cfc83e7fe01713374272dac08c27413b5bb0be84
Eleazar Castilleja <eleazarcastilleja1237@gmail.com>
2026-09-10 18:58:22 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* * Name: Your Full Name 
+* Course: CSCI 1250, Section 001 
+* Assignment: Lab 0, Hello World 
+* Date: September 8, 2026 
+* Description: Prints a greeting and a short introduction to the console. */ 
+
+Console.WriteLine("Hello, World!"); 
+Console.WriteLine("My name is Eleazar Castilleja."); 
+Console.WriteLine("This is my first program at ETSU.");
